ce8dde6983d765582584c135d05039be1c6e006c
Rishabh <rishabh.official.53043@gmail.com>
2024-04-15 21:35:10 +0530

    update

---
+++ vitReact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+ 
+
+  return (
+   <h1>rishabh \ saini</h1>
+  )
+}
+
+export default App
---
+++ vitReact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>hello World i am learning MERN</h3>
+    )
+}
+
+export default Chai
